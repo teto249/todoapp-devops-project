17646b86ab67e949a24c0284e02a2be13d4b691c
teto249 <118367287+teto249@users.noreply.github.com>
2025-07-24 20:41:48 +0800

    comit

---
+++ git init.ps1
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit: Spring Boot Todo App with DevOps setup"
+git branch -M main
+git remote add origin https://github.com/teto249/todoapp-devops-project
+git push -u origin main
